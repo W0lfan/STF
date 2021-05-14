afbfbf8a114b97ba128351c4b4c1baecbbb653b7
W0lfan <69418024+W0lfan@users.noreply.github.com>
2021-05-14 11:37:14 +0200

    Update Starblast arena (game mode).js

--- Starblast arena (game mode).js
+++ Starblast arena (game mode).js
@@ -853,8 +853,7 @@ var tick = function(game) {
           ship.setUIComponent(scoreboard);
           ship.setUIComponent(batteryBox);
           ship.setUIComponent(player_number);
-          ship.instructorSays(
-        "Welcome to SA. After the mining phase you will be teleported on the map and will have to fight players and aliens. Fight and don’t die. Win points with kills, make your team winning 25 000 points."        )
+          ship.instructorSays("Welcome to SA. After the mining phase you will be teleported on the map and will have to fight players and aliens. Fight and don’t die. Win points with kills, make your team winning 25 000 points."        )
           ship.custom.time_start_before_hiding = true;
           ship.custom.timeZ = seconds_beifre_hiding;
           actualize_seconds(ship, ship.custom.secondsToHavePower + "s", "#AD1414");
